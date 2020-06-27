19edf40a19fcc392b047e95ffb2984740c5f614e
baiwenlong <baiwenlong@58.com>
2020-06-28 01:21:20 +0800

    优化拦截代码

--- app/src/main/java/com/example/safetoast/App.kt
+++ app/src/main/java/com/example/safetoast/App.kt
@@ -1,55 +1,10 @@
 package com.example.safetoast
 
 import android.app.Application
-import android.content.Context
-import android.os.Build
-import android.util.Log
-import android.view.Display
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
 
 class App : Application() {
-    @Volatile
-    private var mWindowManager: WindowManager? = null
 
     override fun getSystemService(name: String): Any? {
-        if (Build.VERSION.SDK_INT == 25) {// 兼容android 7.1.1 toast崩溃问题
-            if (name == Context.WINDOW_SERVICE) {
-                if (mWindowManager == null) {
-                    mWindowManager = WindowManagerWrapper(
-                        super.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-                    )
-                }
-                return mWindowManager
-            }
-        }
-        return super.getSystemService(name)
-    }
-
-    class WindowManagerWrapper(private val baseManager: WindowManager) : WindowManager {
-        override fun getDefaultDisplay(): Display {
-            return baseManager.defaultDisplay
-        }
-
-        override fun addView(view: View?, params: ViewGroup.LayoutParams?) {
-            try {
-                baseManager.addView(view, params)
-            } catch (e: WindowManager.BadTokenException) {
-                Log.w("bwl", "add window failed", e)
-            }
-        }
-
-        override fun updateViewLayout(view: View?, params: ViewGroup.LayoutParams?) {
-            baseManager.updateViewLayout(view, params)
-        }
-
-        override fun removeView(view: View?) {
-            baseManager.removeView(view)
-        }
-
-        override fun removeViewImmediate(view: View?) {
-            baseManager.removeViewImmediate(view)
-        }
+        return SafeToastService.getSystemService(name, super.getSystemService(name))
     }
 }
---
+++ app/src/main/java/com/example/safetoast/SafeToastService.kt
@@ -0,0 +1,75 @@
+package com.example.safetoast
+
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import android.view.Display
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+
+/**
+ * Created by baiwenlong on 2020/6/28
+ */
+object SafeToastService {
+    @Volatile
+    private var mWindowManager: WindowManager? = null
+
+    fun getSystemService(name: String, baseService: Any?): Any? {
+        if (Build.VERSION.SDK_INT == 25) {// 兼容android 7.1.1 toast崩溃问题
+            if (name == Context.WINDOW_SERVICE && callFromToast()) {
+                if (mWindowManager == null) {
+                    mWindowManager = WindowManagerWrapper(
+                        baseService as WindowManager
+                    )
+                }
+                return mWindowManager
+            }
+        }
+        return baseService
+    }
+
+    private fun callFromToast(): Boolean {
+        var fromToast = false
+        try {
+            // android.widget.Toast$TN.handleShow
+            val traces = Thread.currentThread().stackTrace
+            if (traces != null) {
+                for (trace in traces) {
+                    if ("android.widget.Toast\$TN" == trace.className && "handleShow" == trace.methodName) {
+                        fromToast = true
+                        break
+                    }
+                }
+            }
+        } catch (ignored: Throwable) {
+        }
+        return fromToast
+    }
+
+    class WindowManagerWrapper(private val baseManager: WindowManager) : WindowManager {
+        override fun getDefaultDisplay(): Display {
+            return baseManager.defaultDisplay
+        }
+
+        override fun addView(view: View?, params: ViewGroup.LayoutParams?) {
+            try {
+                baseManager.addView(view, params)
+            } catch (e: WindowManager.BadTokenException) {
+                Log.w("bwl", "add window failed", e)
+            }
+        }
+
+        override fun updateViewLayout(view: View?, params: ViewGroup.LayoutParams?) {
+            baseManager.updateViewLayout(view, params)
+        }
+
+        override fun removeView(view: View?) {
+            baseManager.removeView(view)
+        }
+
+        override fun removeViewImmediate(view: View?) {
+            baseManager.removeViewImmediate(view)
+        }
+    }
+}
