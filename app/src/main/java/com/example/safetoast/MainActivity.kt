4e13510bcaaf3cde0440c6fb8562c95d8ae364df
WL <echowenlong@gmail.com>
2020-06-28 00:07:56 +0800

    add codes

---
+++ app/src/main/java/com/example/safetoast/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.safetoast
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show()
+        Thread.sleep(5000)
+    }
+}
